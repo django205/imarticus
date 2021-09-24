2a016ab5882ef16ad2b2dfd14257b52b1a0250c5
Yogesh Kumar <yogeshkumar@Yogeshs-MacBook-Air.local>
2021-09-24 20:34:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const url = `mongodb+srv://yogesh:yogesh@cluster0.h0itg.mongodb.net/imarticus?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
